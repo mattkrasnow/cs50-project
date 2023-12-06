f2ef39365c25ffa364508377230b72ee55329b0a
mwagner6 <max.wagner.us@gmail.com>
2023-12-06 00:05:25 -0500

    fixed hp bar going under 0

--- hpbar.py
+++ hpbar.py
@@ -8,5 +8,6 @@ class HPBar(object):
         self.h = 10
     
     def display(self, screen, centerX, centerY):
-        pygame.draw.rect(screen, (0, 0, 0), (centerX - self.w/2, centerY - self.h/2, self.w, self.h))
-        pygame.draw.rect(screen, (255 - 255*(self.hp/self.maxHp), 255*(self.hp/self.maxHp), 0), (centerX - self.w/2 + 2, centerY - self.h/2 + 2, self.hp * (self.w - 4) / self.maxHp, self.h - 4))
+        if self.hp > 0:
+            pygame.draw.rect(screen, (0, 0, 0), (centerX - self.w/2, centerY - self.h/2, self.w, self.h))
+            pygame.draw.rect(screen, (255 - 255*(self.hp/self.maxHp), 255*(self.hp/self.maxHp), 0), (centerX - self.w/2 + 2, centerY - self.h/2 + 2, self.hp * (self.w - 4) / self.maxHp, self.h - 4))
