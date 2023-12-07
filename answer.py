3b6e2e98c51eb3768f245f4d526445d0fafb1100
mwagner6 <max.wagner.us@gmail.com>
2023-12-07 15:29:07 -0500

    main and answer commented

--- answer.py
+++ answer.py
@@ -5,13 +5,13 @@ import math
 class Answer():
     # constructor that sets the answer objects
     def __init__(self, input, x, y, isCorrect):
-        self.answerText = input
+        self.answerText = input # sets the text above the portal if this exit is for a multiple choice question
         self.w = 60
         self.h = 150
         self.x = x - self.w/2
         self.y = y - self.h/2
         self.r = max(self.w, self.h)
-        self.correctness = isCorrect
+        self.correctness = isCorrect # if this is a multiple choice question, store whether this is the correct or incorrect answer (exits on normal levels always have a value of True)
         self.cycle = 0
 
     def duckCollision(self, duck):
@@ -26,12 +26,11 @@ class Answer():
         font = pygame.font.Font('SourceCodePro-SemiBold.ttf', 10)
         text = font.render(self.answerText, True,  (0,255,0), (50,50,50))
         aRect = text.get_rect()
-        aRect.center = (self.x + self.w/2, self.y - 25)
-        screen.blit(text, aRect)
-        self.cycle += 1
+        aRect.center = (self.x + self.w/2, self.y - 25) # center the text above the portal
+        screen.blit(text, aRect) # draw the text onto the screen
+        self.cycle += 1 # increment cycle for the portal effect
         temp = pygame.Surface((self.w, self.h))
-        for i in range(self.r):
+        for i in range(self.r): # to get the portal effect, a number of circles are drawn onto a separate pygame "surface" which is the exact dimensions of the portal, which is then put onto the main screen
             currentColor = (0, 127*(math.sin((self.cycle+i)/25) + 1), 255)
             pygame.draw.circle(temp, currentColor, (self.w/2, self.h/2), self.r-i)
         screen.blit(temp, (self.x, self.y))
-        #pygame.draw.rect(screen, (255*random.random(),255*random.random(),255*random.random()), (self.x, self.y, self.w, self.h))
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from textscreen import TextScreen
 
 pygame.init()
 
-
+# set the variables used in main
 BACKGROUND = (50,50,50)
 btime = 15
 hitCooldown = 60
@@ -28,15 +28,17 @@ pygame.display.set_caption('CS50 Duck to the Rescue')
 
 # intialize duck
 duck = Duck(0, HEIGHT-150, WIDTH, HEIGHT)
+# initialize the clock that keeps game speed consistent
 clock = pygame.time.Clock()
+# create the array of levels and level-like objects that is iterated through to run the game 
 levels = [
-    TextScreen(['The Year is 2024...', "it is set to be CS50's biggest yet", 'But there is one problem', 'The evil coders at MIT have hacked the code!', 'In all the chaos, only one man can save CS50', 'Or, as it would be, one duck.'], WIDTH, HEIGHT),
+    TextScreen(['The Year is 2024...', "it is set to be CS50's biggest yet", 'But there is one problem', 'The evil coders at MIT have hacked the code!', 'In all the chaos, only one man can save CS50', 'Or, as it would be, one duck.'], WIDTH, HEIGHT), # TextScreen objects take in a list of strings to make a Star-Wars-like scrolling text screen
     TextScreen(['After hacking into the MIT mainframe,', 'the duck discovers something sinister...', 'It is a maze filled with evil beavers!', 'He must reach the portals on each level', 'Defeat the beavers,', 'and answer questions to save the psets!'], WIDTH, HEIGHT),
-    Level([], [Block(500, HEIGHT - 100, 20, 100)], 900, 500, WIDTH, HEIGHT, 100, 400, disptext="Use the A and D keys to move around the level, and the W key to jump!"),
+    Level([], [Block(500, HEIGHT - 100, 20, 100)], 900, 500, WIDTH, HEIGHT, 100, 400, disptext="Use the A and D keys to move around the level, and the W key to jump!"), # each level takes an array of enemy position, block positions, a player spawn point, and the location of an exit portal, as well as text to display
     Level([[750, HEIGHT - 400]], [Block(500, HEIGHT - 100, 300, 20), Block(800, HEIGHT - 200, 100, 20)], 850, HEIGHT - 400, WIDTH, HEIGHT, 100, 400, disptext="Click to shoot at evil beavers, and don't get hit!"),
     Level([], [], 850, HEIGHT - 100, WIDTH, HEIGHT, 100, 400, disptext="To recover each pset, you must answer a question correctly. Getting it right on the first try increases your damage!"),
-    McQuestion('In Scratch, what is the purpose of a loop?', ['Drawing a circle', 'Repeating code', 'Moving in a circle', 'There are no loops'], 1, WIDTH, HEIGHT),
-    Level([[750, HEIGHT - 400], [700, HEIGHT - 400], [650, HEIGHT - 400], [600, HEIGHT - 400]], [Block(200, HEIGHT - 200, WIDTH - 200, 20), Block(0, HEIGHT - 100, 50, 20)], 850, HEIGHT - 400, WIDTH, HEIGHT, 100, 400, disptext="The MIT beavers aren't that smart, so try to take advantage!"),
+    McQuestion('In Scratch, what is the purpose of a loop?', ['Drawing a circle', 'Repeating code', 'Moving in a circle', 'There are no loops'], 1, WIDTH, HEIGHT), # question levels take in a question, a list of answers, and the index of the correct answer
+    Level([[750, HEIGHT - 400], [700, HEIGHT - 400], [650, HEIGHT - 400], [600, HEIGHT - 400]], [Block(200, HEIGHT - 200, WIDTH - 200, 20), Block(0, HEIGHT - 100, 50, 20)], 850, HEIGHT - 400, WIDTH, HEIGHT, 100, 400, disptext="The MIT beavers aren't that smart, so try to take advantage!"), 
     McQuestion('Which is the correct header for a loop in c?', ['for(i = 0, i < 5; i++)', 'for(int i = 0; i < 5; i++)', 'do while():', 'for i in range(5)'], 1, WIDTH, HEIGHT),
     Level([[750, HEIGHT - 200], [700, HEIGHT - 200]], [Block(0, HEIGHT - 200, 200, 20), Block(200, HEIGHT - 150, 200, 20), Block(400, HEIGHT - 100, 200, 20), Block(600, HEIGHT - 50, 200, 20)], 850, HEIGHT - 150, WIDTH, HEIGHT, 0, 200, disptext="Be careful, the beavers can jump too!"),
     McQuestion('Which is NOT a property of arrays in c?', ['Stores a list of items', 'Stores a single type', 'Has dynamic size', 'Can be multidimensional'], 2, WIDTH, HEIGHT),
@@ -53,23 +55,24 @@ levels = [
     Level([], [], 900, HEIGHT - 150, WIDTH, HEIGHT, 0, HEIGHT - 150, "The duck slows down. It knows the next question is the last pset"),
     McQuestion('What is NOT a language used when using Flask?', ['JavaScript', 'HTML', 'CSS', 'C'], 3, WIDTH, HEIGHT),
     TextScreen(['The duck breathes heavily.', 'It knows it has finished all the psets,', 'but it still feels like something is missing.', 'It hears some sounds from just a little further,', 'and realizes what he forgot about.', 'The final project!', 'Somehow, MIT has captured all the old final projects,', 'and placed its strongest beaver to protect them.', "This will be the duck's hardest battle yet", 'Good luck.'], WIDTH, HEIGHT),
-    Boss(WIDTH, HEIGHT),
+    Boss(WIDTH, HEIGHT), # the boss level is the same each time, with some randomness in the constructor. It doesn't need any inputs other than the screen dimensions, which all levels use
     TextScreen(["Congratulations, you have saved CS50!", "With the beavers defeated,", "The duck recovers the rest of the code", "He rushes home and puts it in place,", "starting codespaces minutes before Lecture 0.", "CS50 is saved!"], WIDTH, HEIGHT)
 ]
-run = True
+
 bullets = []
-currentLevel = 0
-while run:
-    if levels[currentLevel].levelComplete:
-        currentLevel += 1
+currentLevel = 0 # keep track of which index of the levels in the array the player is currently on
+while True:
+    if levels[currentLevel].levelComplete: # check for level completion conditions (touching the portal, or the scrolling text finishing)
+        currentLevel += 1 # increment the next level, reset all of its variables to the default values in case the player has tried the level before, and put the duck in the correct spawn position. Finally, the level's enemies are repopulated
         levels[currentLevel].levelComplete = False
         levels[currentLevel].levelFailed = False
         duck.x = levels[currentLevel].spawnX
         duck.y = levels[currentLevel].spawnY
         duck.yvel = 0
         duck.vel = 0
-    if levels[currentLevel].levelFailed:
-        currentLevel -= 1
+        levels[currentLevel].populateEnemies()
+    if levels[currentLevel].levelFailed: # check if the level is failed (this only happens when the player chooses the wrong gate on a question)
+        currentLevel -= 1 # decrement the level, and reset all of its variables to the default values, as well as repopulating any enemies that were on the level. This ensures that there will be the proper amount of enemies on the level.
         levels[currentLevel].levelComplete = False
         levels[currentLevel].levelFailed = False
         duck.x = levels[currentLevel].spawnX
@@ -77,48 +80,48 @@ while run:
         levels[currentLevel].populateEnemies()
         duck.yvel = 0
         duck.vel = 0
-    clock.tick(60)
-    duck.move()
+    clock.tick(60) # this makes the game loop run at a consistent 60 frames per second. 
+    duck.move() # call the duck movement, which takes player controls and updates the duck's position with their input and gravity. 
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            run = False # if the player manually closes the game, pygame ends the loop.
    
-    #draw the duck in the game
-    screen.fill(BACKGROUND)
+    screen.fill(BACKGROUND) # fill in the background with our predefined color
     
     duck.jumping = True
-    firstRight = levels[currentLevel].levelPhysics(duck)
-    if firstRight:
+    firstRight = levels[currentLevel].levelPhysics(duck) # runs the level physics on the duck. In most cases, this will return false and calculate block collisions, enemy collisions, portal collisions, and more depending on the level type.
+    if firstRight: # if the function returns true, which only happens if the player gets a question right on their first try, their gun will gain more damage.
         bulletDamage += 1
     
-    levels[currentLevel].display(screen)
+    levels[currentLevel].display(screen) # displays the levels, which includes blocks, portals, and enemies. In the boss level, this also draws the boss. 
 
-    levels[currentLevel].bulletCollisions(bullets, bulletDamage)
+    levels[currentLevel].bulletCollisions(bullets, bulletDamage) # calculates bullet collisions with enemeis and bosses depending on the level type.v
 
 
-    #draw a bullet at the location of the duck
+    # increment timer for between bullet shots
     if btime < 30:
         btime += 1
-    if pygame.mouse.get_pressed()[0] and btime == 30:
+
+    if pygame.mouse.get_pressed()[0] and btime == 30: # check if player is trying to shoot, and if they have waited enough time
         btime = 0
         bulletX = duck.guntipX
         bulletY = duck.guntipY
-        mX, mY = pygame.mouse.get_pos()
+        mX, mY = pygame.mouse.get_pos() # get mouse position
         xDiff = mX - bulletX
         yDiff = mY - bulletY
-        normFactor = math.sqrt(xDiff * xDiff + yDiff * yDiff)
+        normFactor = math.sqrt(xDiff * xDiff + yDiff * yDiff) # normalize difference between mouse position and bullet so it moves at the same velocity in any direction
         xDiff = 8 * xDiff / normFactor
         yDiff = 8 * yDiff / normFactor
-        bullets.append(Bullet(bulletX, bulletY, xDiff, yDiff))
+        bullets.append(Bullet(bulletX, bulletY, xDiff, yDiff)) # add a bullet to our list of bullets
 
-    if hitCooldown < 60:
+    if hitCooldown < 60: # increment timer between times player can be hit
         hitCooldown += 1
-    if levels[currentLevel].enemyHit and hitCooldown == 60:
+    if levels[currentLevel].enemyHit and hitCooldown == 60: # if the enemy is currently hitting the player, and the player is able to be hit currently, the player loses some hp
         hitCooldown = 0
         hp -= 1
         duck.hp = hp
-        if levels[currentLevel].hitDirection == 'r':
+        if levels[currentLevel].hitDirection == 'r': # check what direction the hit came from. Move the player away and bounce off the ground a little bit
             duck.jumping = True
             duck.y -= 8
             duck.yvel += 5
@@ -128,26 +131,26 @@ while run:
             duck.y -= 8
             duck.yvel += 5
             duck.vel = -5
-        if hp == 0:
+        if hp == 0: # if the player is out of HP, reset to the first level, and reset the player bullet damage as well. Since text scroller clock is not reset, those levels will not appear again
             bulletDamage = 1
             currentLevel = 0
-            levels[currentLevel].levelComplete = False
+            levels[currentLevel].levelComplete = False # additionally, run all the same level reset as when advancing one level or moving back one level
             hp = 10
             duck.hp = hp
-            levels[currentLevel].populateEnemies()
+            levels[currentLevel].populateEnemies() 
             duck.vel = 0
             duck.yvel = 0
             duck.x = levels[currentLevel].spawnX
             duck.y = levels[currentLevel].spawnY
         
-    for bullet in bullets:
+    for bullet in bullets: # display and move each bullet. Additionally, check if it has moved off the screen, and if so, delete it
         bullet.display(screen)
         bullet.move()
         if bullet.x < -100 or bullet.x > WIDTH + 100 or bullet.y < -100 or bullet.y > HEIGHT + 100:
             bullets.pop(bullets.index(bullet))
-    duck.display(screen)
+    duck.display(screen) # draw the duck onto the screen
 
-    pygame.display.update()
+    pygame.display.update() # update the display so the player sees everything drawn this loop. 
 
-pygame.quit()
+pygame.quit() # exit pygame and end the code
 sys.exit()
