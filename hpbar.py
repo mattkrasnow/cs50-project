e686bbdaf3673f1f71417bb0e0febdbb9dd666de
mwagner6 <max.wagner.us@gmail.com>
2023-12-07 16:14:02 -0500

    commented hpbar

--- hpbar.py
+++ hpbar.py
@@ -5,11 +5,11 @@ class HPBar(object):
     def __init__(self, hpamt):
         self.maxHp = hpamt
         self.hp = hpamt
-        self.w = 10 * self.maxHp
+        self.w = 10 * self.maxHp # define the width and height of the hp bar based on the initial hp
         self.h = 10
     
     # display the HP bar on screen at the set position, which in all cases if above entities' heads 
     def display(self, screen, centerX, centerY):
         if self.hp > 0:
-            pygame.draw.rect(screen, (0, 0, 0), (centerX - self.w/2, centerY - self.h/2, self.w, self.h))
-            pygame.draw.rect(screen, (255 - 255*(self.hp/self.maxHp), 255*(self.hp/self.maxHp), 0), (centerX - self.w/2 + 2, centerY - self.h/2 + 2, self.hp * (self.w - 4) / self.maxHp, self.h - 4))
+            pygame.draw.rect(screen, (0, 0, 0), (centerX - self.w/2, centerY - self.h/2, self.w, self.h)) # draw a black background rectangle so the hp bar stands out
+            pygame.draw.rect(screen, (255 - 255*(self.hp/self.maxHp), 255*(self.hp/self.maxHp), 0), (centerX - self.w/2 + 2, centerY - self.h/2 + 2, self.hp * (self.w - 4) / self.maxHp, self.h - 4)) # modulate color based on the amount of hp left vs. total hp, and draw a rectangle of the correct width
