16d2d7a768fbe13b6d8b570776461172106c423d
mwagner6 <max.wagner.us@gmail.com>
2023-12-07 16:12:34 -0500

    commented enemy and enemy bullet

--- enemy.py
+++ enemy.py
@@ -12,14 +12,14 @@ class Enemy(object):
         self.yaccel = 0.3
         self.yvel = 0
         self.hp = 6
-        self.maxHp = 6
+        self.maxHp = 6 # initialize an hp bar for the enemy
         self.hpBar = HPBar(6)
 
-        beaverImg = pygame.image.load("beaversprite.png").convert_alpha()
+        beaverImg = pygame.image.load("beaversprite.png").convert_alpha() # load in enemy image and size it correctly
         self.imgL = pygame.transform.scale_by(beaverImg, 0.3)
         self.imgR = self.imgL.copy()
-        self.imgR = pygame.transform.flip(self.imgR, flip_x=True, flip_y=False)
-        self.h = self.imgR.get_height()
+        self.imgR = pygame.transform.flip(self.imgR, flip_x=True, flip_y=False) # flip enemy image to face the player
+        self.h = self.imgR.get_height() # get enemy width and height for collisions
         self.w = self.imgR.get_width()
         self.dir = 'r'
         self.screenwidth = screenwidth
@@ -27,19 +27,19 @@ class Enemy(object):
 
     # displays the MIT badger on screen, facing the duck
     def display(self, win):
-        if self.dir == 'r':
+        if self.dir == 'r': # draw enemy beaver facing left or right, depending on what direction it is facing
             win.blit(self.imgR, (self.x, self.y))
         else:
             win.blit(self.imgL, (self.x, self.y))
         self.hpBar.hp = self.hp
-        self.hpBar.display(win, self.x + self.w/2, self.y - 30)
+        self.hpBar.display(win, self.x + self.w/2, self.y - 30) # update hp and draw the hpbar
     
     # move the enemy in the direction of the duck
     def move(self, duck):
-        if self.y >= self.screenheight - self.h:
+        if self.y >= self.screenheight - self.h: # make beavers stop falling at bottom of screen, and allow them to jump there. 
             self.y = self.screenheight - self.h
             self.jumping = False
-        if duck.x + duck.w/2 > self.x + self.w/2:
+        if duck.x + duck.w/2 > self.x + self.w/2: # check if the duck is to the left or right of the enemy, and face towards it (as if a platyer is pressing A or D)
             self.dir = 'r'
             if self.x < self.screenwidth - self.w:
                 self.vel += self.xaccel
@@ -47,25 +47,24 @@ class Enemy(object):
             self.dir = 'l'
             if self.x > 10:
                 self.vel -= self.xaccel
-        if duck.y + duck.h < self.y + self.h and self.jumping == False:
+        if duck.y + duck.h < self.y + self.h and self.jumping == False: # if the duck is above the enemy, and it is able to jump, it jumps
             self.jumping = True
             self.yvel = 10
-        if self.jumping == True:
+        if self.jumping == True: # as long as the enemy is jumping, it accelerates downwards and moves based on its vertical velocity
             self.yvel -= self.yaccel
             self.y -= self.yvel  
-        
 
-        if self.jumping == False:
+        if self.jumping == False: # enemies also have friction on the ground
             self.vel *= 0.95
-        self.x += self.vel
-        if self.x <= 0:
+        self.x += self.vel # move based on x velocity in the x direction
+        if self.x <= 0: # collision with the left side of the screen
             self.x = 0
             self.vel = 0
-        if self.x >= self.screenwidth - self.w:
+        if self.x >= self.screenwidth - self.w: # collision with the right side of the screen
             self.x = self.screenwidth - self.w
             self.vel = 0
     # determine if the enemy is touching the duck    
     def duckCollision(self, duck):
-        if duck.w + duck.x > self.x and duck.x < self.x + self.w and duck.y + duck.h > self.y and duck.y < self.y + self.h:
+        if duck.w + duck.x > self.x and duck.x < self.x + self.w and duck.y + duck.h > self.y and duck.y < self.y + self.h: # use rectangle collision to detect when the player and the enemy are colliding. 
             return True
         return False
--- enemybullet.py
+++ enemybullet.py
@@ -8,16 +8,16 @@ class EnemyBullet(object):
         self.y = y
         self.xvel = xvel
         self.yvel = yvel
-        bulletImg = pygame.image.load("mitbullet.png").convert_alpha()
+        bulletImg = pygame.image.load("mitbullet.png").convert_alpha() # load in the MIT bullet sprite and resize it
         self.img = pygame.transform.scale_by(bulletImg, 0.02)
         angle = math.atan(self.yvel/self.xvel)
-        self.img = pygame.transform.rotate(self.img, 360-math.degrees(angle))
-        self.w = self.img.get_width()
+        self.img = pygame.transform.rotate(self.img, 360-math.degrees(angle)) # rotate the image so it faces the correct direction towards the player
+        self.w = self.img.get_width() # get the bullet's width and height for collisions
         self.h = self.img.get_height()
 
     # displays the Boss's bullet on screen as the MIT logo
     def display(self, screen):
-        screen.blit(self.img, (self.x, self.y))
+        screen.blit(self.img, (self.x, self.y)) # draw the image onto the screen
     
     # move the bullet with respect to its velocity
     def move(self):
@@ -26,6 +26,6 @@ class EnemyBullet(object):
         
     # determine if the bullet is touching the enemy (the duck)
     def enemyCollision(self, enemy):
-        if enemy.w + enemy.x > self.x and enemy.x < self.x + self.w and enemy.y + enemy.h > self.y and enemy.y < self.y + self.h:
+        if enemy.w + enemy.x > self.x and enemy.x < self.x + self.w and enemy.y + enemy.h > self.y and enemy.y < self.y + self.h: # use rectangle collision to see when enemy bullets collide with the player.
             return True
         return False
