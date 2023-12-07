8cfef4fdfce5edc9c73b6088ea07b9b8dd2bfb11
mwagner6 <max.wagner.us@gmail.com>
2023-12-07 16:22:56 -0500

    commented mcQuestion

--- mcQuestion.py
+++ mcQuestion.py
@@ -9,13 +9,13 @@ class McQuestion():
     def __init__ (self, question, answers, correctIndex, screenWidth, screenHeight):
         self.spawnX = screenWidth/2
         self.spawnY = 200
-        blocks = [Block(0, screenHeight-350, 200, 20), Block(screenWidth - 200, screenHeight-350, 200, 20), Block(screenWidth/2 - 100, screenHeight-200, 200, 20), Block(screenWidth/2-200, screenHeight-50, 100, 20), Block(screenWidth/2+100, screenHeight-50, 100, 20)]
-        self.level = Level([], blocks, 100000, 100000, screenWidth, screenHeight, self.spawnX, self.spawnY, disptext=question)
+        blocks = [Block(0, screenHeight-350, 200, 20), Block(screenWidth - 200, screenHeight-350, 200, 20), Block(screenWidth/2 - 100, screenHeight-200, 200, 20), Block(screenWidth/2-200, screenHeight-50, 100, 20), Block(screenWidth/2+100, screenHeight-50, 100, 20)] # predefined blocks for question levels
+        self.level = Level([], blocks, 100000, 100000, screenWidth, screenHeight, self.spawnX, self.spawnY, disptext=question) # create a level to run the physics and collisions of the multiple choice question, with no exit (off the screen), and displaying the text of the question
         positions = [[100, 150], [100, screenHeight-100], [screenWidth-100, 150], [screenWidth-100, screenHeight-100]]
         self.answers = []
-        for i in range(4):
+        for i in range(4): # randomize the order of the answers
             ind = random.randint(0, 3-i)
-            self.answers.append(Answer(answers[i], positions[ind][0], positions[ind][1], i == correctIndex))
+            self.answers.append(Answer(answers[i], positions[ind][0], positions[ind][1], i == correctIndex)) # for each answer, add to the array of this level's answers a portal that is either correct or incorrect, and put it in a random unused position
             del positions[ind]
         self.levelComplete = False
         self.levelFailed = False
@@ -26,24 +26,24 @@ class McQuestion():
 
     # display the questions and answers on the screen
     def display(self, screen):
-        self.level.display(screen)
+        self.level.display(screen) # draw the level in, which shows the blocks
 
         for a in self.answers:
-            a.display(screen)
+            a.display(screen) # draw in each of the exits
     
     # define if the duck has answered the question correctly and duck's movement
     def levelPhysics(self, duck):
-        self.level.levelPhysics(duck)
+        self.level.levelPhysics(duck) # run the level physics with the stored level, which handles the player colliding with blocks
         for a in self.answers:
-            if a.duckCollision(duck):
-                if a.correctness:
+            if a.duckCollision(duck): # for every portal, check if the player is touching it
+                if a.correctness: # if the player touches the correct portal, they complete the level, and return True, signaling to power up
                     self.levelComplete = True
                     if self.questionAnswered == False:
-                        self.questionAnswered = True
+                        self.questionAnswered = True # only allow the player to get the power up once
                         return True
                 else:
-                    self.levelFailed = True
-                    self.questionAnswered = True
+                    self.levelFailed = True # fail the level if the player chooses the wrong portal, which will move them back a level
+                    self.questionAnswered = True # only allow the player to power up if they choose the correct answer on the first try
         return False
     
     # Prevents developer errors if the developer treats mcquestion objects as levels
@@ -53,5 +53,3 @@ class McQuestion():
     # Prevents developer errors if the developer treats mcquestion objects as levels
     def populateEnemies(self):
         pass
-
-    # generate a level
