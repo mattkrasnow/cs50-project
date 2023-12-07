3c1679255c3c68eb0ba68367c047c87cc0f8ac9d
mwagner6 <max.wagner.us@gmail.com>
2023-12-07 16:02:33 -0500

    commented bullet

--- bullet.py
+++ bullet.py
@@ -7,14 +7,14 @@ class Bullet(object):
         self.y = y
         self.xvel = xvel
         self.yvel = yvel
-        bulletImg = pygame.image.load("newbullet.png").convert_alpha()
+        bulletImg = pygame.image.load("newbullet.png").convert_alpha() # load in the bullet image, and resize it to the proper size
         self.img = pygame.transform.scale_by(bulletImg, 0.2)
-        self.w = self.img.get_width()
+        self.w = self.img.get_width() # get bullet width and height for collisions from our image
         self.h = self.img.get_height()
 
     # displays the bullet on scrreen as an image
     def display(self, screen):
-        screen.blit(self.img, (self.x, self.y))
+        screen.blit(self.img, (self.x, self.y)) 
     
     # moves the bullet in the direction and speed of the set velocity
     def move(self):
@@ -23,6 +23,6 @@ class Bullet(object):
         
     # determines if the bullet is touching an enemy    
     def enemyCollision(self, enemy):
-        if enemy.w + enemy.x > self.x and enemy.x < self.x + self.w and enemy.y + enemy.h > self.y and enemy.y < self.y + self.h:
+        if enemy.w + enemy.x > self.x and enemy.x < self.x + self.w and enemy.y + enemy.h > self.y and enemy.y < self.y + self.h: # uses rectangle collision to detect if the bullet is colliding with the enemy that was passed in. 
             return True
         return False
